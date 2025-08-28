207ddfa49077ea40ac4dc410c4efea3781739acf
ashongdev <ashongabdalla51@gmail.com>
2025-08-28 22:15:43 +0000

    Update code

--- backend/capstone/settings.py
+++ backend/capstone/settings.py
@@ -10,10 +10,19 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Add this line:
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+# You should already have:
+STATIC_URL = "/static/"
 
 
 # Quick-start development settings - unsuitable for production
