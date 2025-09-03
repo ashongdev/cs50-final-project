37f356e778008e760fef28d21ef050afedfdadf9
ashongdev <ashongabdalla51@gmail.com>
2025-09-03 22:13:58 +0000

    feat: Update SITE_ID to 4 and add responsive layout styles for better mobile experience

--- capstone/settings.py
+++ capstone/settings.py
@@ -36,7 +36,7 @@ ALLOWED_HOSTS = [
 
 # Application definition
 
-SITE_ID = 3
+SITE_ID = 4
 
 INSTALLED_APPS = [
     "django.contrib.admin",
--- final_project/urls.py
+++ final_project/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.index, name="index"),
     path("notes", views.index, name="index"),
     path("all", views.index, name="index"),
     path("archives", views.archives, name="archives"),
